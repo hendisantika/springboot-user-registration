6574a58b0380771e353b587f868ad7b4ce5d20b3
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-05 08:03:48 +0700

    #15 - Add sendConfirmationMail service in UserService

--- src/main/java/com/hendisantika/springbootuserregistration/service/UserService.java
+++ src/main/java/com/hendisantika/springbootuserregistration/service/UserService.java
@@ -2,6 +2,7 @@ package com.hendisantika.springbootuserregistration.service;
 
 import com.hendisantika.springbootuserregistration.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -26,4 +27,17 @@ public class UserService implements UserDetailsService {
     private final ConfirmationTokenService confirmationTokenService;
 
     private final EmailSenderService emailSenderService;
+
+    void sendConfirmationMail(String userMail, String token) {
+        final SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(userMail);
+        mailMessage.setSubject("Mail Confirmation Link!");
+        mailMessage.setFrom("<MAIL>");
+        mailMessage.setText(
+                "Thank you for registering. Please click on the below link to activate your account." + "http" +
+                        "://localhost:8080/sign-up/confirm?token="
+                        + token);
+
+        emailSenderService.sendEmail(mailMessage);
+    }
 }
