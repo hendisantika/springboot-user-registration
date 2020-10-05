4af987726aef19a244d9af7718b9d71e94471b13
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-05 07:59:28 +0700

    #9 - Add ConfirmationToken Entity

---
+++ src/main/java/com/hendisantika/springbootuserregistration/entity/ConfirmationToken.java
@@ -0,0 +1,49 @@
+package com.hendisantika.springbootuserregistration.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-user-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/10/20
+ * Time: 07.58
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class ConfirmationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String confirmationToken;
+
+    private LocalDate createdDate;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    ConfirmationToken(User user) {
+        this.user = user;
+        this.createdDate = LocalDate.now();
+        this.confirmationToken = UUID.randomUUID().toString();
+    }
+}
