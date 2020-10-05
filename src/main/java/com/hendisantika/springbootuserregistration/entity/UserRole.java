214b2fd33512f182b5c0410151fe094908396669
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-05 07:56:56 +0700

    #6 - Add UserRole Enum

---
+++ src/main/java/com/hendisantika/springbootuserregistration/entity/UserRole.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootuserregistration.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-user-registration
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/10/20
+ * Time: 07.56
+ */
+public enum UserRole {
+    ADMIN, USER
+}
