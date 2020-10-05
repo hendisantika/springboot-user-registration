6e34013a08efcd526f172c7a178080cac8b7e710
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-05 07:51:37 +0700

    #1 - Add required libs

---
+++ src/main/java/com/hendisantika/springbootuserregistration/SpringbootUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootuserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUserRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootuserregistration/SpringbootUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootuserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
